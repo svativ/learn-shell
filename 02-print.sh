84ed64941768a650e3bcff93a2e9ad779f0973da
svati <svativangapandu9@gmail.com>
2023-08-24 17:38:37 +0530

    echo command

--- 02-print.sh
+++ 02-print.sh
@@ -1,2 +1,18 @@
 echo Hello World
 
+# Red      31
+# Green    32
+# Yellow   33
+# Blue     34
+# Magenta  35
+# cyan     36
+
+# Syntax
+# echo -e "\[CODEmMESSAGE\e[0m"
+
+echo -e "\e[31mHello World\e[0m"
+echo -e "\e[32mHello World\e[0m"
+echo -e "\e[33mHello World\e[0m"
+echo -e "\e[34mHello World\e[0m"
+echo -e "\e[35mHello World\e[0m"
+echo -e "\e[36mHello World\e[0m"
