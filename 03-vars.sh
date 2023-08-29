22f57e0f5dedd28c9e3e8f0e77fadc3f2b467717
svati <svativangapandu9@gmail.com>
2023-08-29 12:24:53 +0530

    Password NFRS

--- 03-vars.sh
+++ 03-vars.sh
@@ -1,2 +1,16 @@
 a=10
 echo a is $a
+
+# special variables
+# $0 -$N, $*, $#
+
+# substitution Variables
+## Command Substitution
+DATE=$(date)
+
+echo Today Date is $DATE
+
+#Arithemetic substitution
+ADD=$((2+2))
+echo ADD of 2+2 = $ADD
+
