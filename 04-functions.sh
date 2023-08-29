ca1bca20bd64783d8d3effbca6cbd0f541396e91
svati <svativangapandu9@gmail.com>
2023-08-29 12:57:15 +0530

    Password NFR

--- 04-functions.sh
+++ 04-functions.sh
@@ -7,3 +7,20 @@ greeting() {
 
 greeting
 echo Function Exit Status - $?
+
+# you declare var in main program, you can access that in function and vive-versa.
+# Function have its own special variables
+input() {
+  echo First Argument -$1
+  echo second Argument -$2
+  echo ALL Arguments -$*
+  echo  No of Arguments -$#
+}
+
+input abc 1234
+
+
+
+
+
+
